97fe38c883b6fe1ebf9ff5b72d96bec4bfde3b7e
yushuoliu1015 <122757270+yushuoliu1015@users.noreply.github.com>
2024-03-25 11:32:25 -0400

    Update signin-form.php

---
+++ public/form/signin-form.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <h2>Login Form</h2>
+    <form action="signin.php" method="post">
+        <div>
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" required>
+        </div>
+        <div>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
+        </div>
+        <div>
+            <input type="submit" value="Login">
+        </div>
+    </form>
+</body>
+</html>
