372497bf012db96fb1d3f92287e9a5eae7ffc80d
Mohammad Saimur Rashid <rashidtusharr@gmail.com>
2024-04-12 10:31:29 -0400

    added a popup when user succesfully loggs in

--- public/src/features/login_process.php
+++ public/src/features/login_process.php
@@ -3,7 +3,7 @@ session_start();
 
 // Check if username and password are set
 if (!isset($_POST['username'], $_POST['password'])) {
-    header("Location: login.php?error=Please%20fill%20in%20both%20username%20and%20password");
+    header("Location: ../../form/login.php?error=Please%20fill%20in%20both%20username%20and%20password");
     exit;
 }
 
@@ -40,21 +40,24 @@ if ($result->num_rows == 1) {
 
         // Check if $_SESSION['registrationOrder'] is set correctly
         if (isset($_SESSION['registrationOrder'])) {
-            header("Location: ../../levels/level1.php"); // Redirect to level 1
+            // Redirect to level1 after successful login
+        echo '<script>alert("Successful logged in!");</script>';
+        echo '<script>window.location.href = "../../levels/level1.php";</script>';
+       
             exit;
         } else {
             // If registration order is not set, redirect back to login with an error message
-            header("Location: login.php?error=Sorry,%20an%20error%20occurred.%20Please%20try%20logging%20in%20again.");
+            header("Location: ../../form/login.php?error=Sorry,%20an%20error%20occurred.%20Please%20try%20logging%20in%20again.");
             exit;
         }
     } else {
         // Incorrect password
-        header("Location: login.php?error=Sorry,%20the%20username%20or%20password%20is%20incorrect!");
+        header("Location: ../../form/login.php?error=Sorry,%20the%20username%20or%20password%20is%20incorrect!");
         exit;
     }
 } else {
     // No user found with that username
-    header("Location: login.php?error=Sorry,%20the%20username%20or%20password%20is%20incorrect!");
+    header("Location: ../../form/login.php?error=Sorry,%20the%20username%20or%20password%20is%20incorrect!");
     exit;
 }
 
